0708924a256210e5472884bb70c746203c572e3d
dmitriy <dmitriy.krista@gmail.com>
2015-06-10 12:54:35 +0000

    initial commit

---
+++ bot.php
@@ -0,0 +1,115 @@
+<?php
+
+require_once "Spyc.php";
+
+class PingThread extends Thread {
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public function run()
+    {
+        while(true) {
+            $session = curl_init();
+            curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($session, CURLOPT_TIMEOUT, 10);
+            curl_setopt($session, CURLOPT_CONNECTTIMEOUT, 10);
+            curl_setopt($session, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($session, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($session, CURLOPT_URL, "https://csgo.tm/api/PingPong/?key=" . $this->config['secret_key']);
+            curl_exec($session);
+            sleep(150);
+        }
+    }
+}
+
+
+class WorkerThread extends Thread {
+
+    private $items;
+    private $config;
+
+    public function __construct($items, $config)
+    {
+        $this->items = $items;
+        $this->config = $config;
+    }
+
+    public function run()
+    {
+        $session = curl_init();
+        curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($session, CURLOPT_TIMEOUT, 10);
+        curl_setopt($session, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($session, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($session, CURLOPT_SSL_VERIFYPEER, 0);
+
+        while(true) {
+            foreach ($this->items as $item) {
+                curl_setopt($session, CURLOPT_URL, "https://csgo.tm/api/ItemInfo/{$item['classid']}_{$item['instanceid']}/ru/?key=1");
+                $data = json_decode(curl_exec($session), true);
+                if (json_last_error() != 0) {
+                    echo "json_last_error - " . json_last_error() . "\n";
+                } else {
+                    foreach($data['offers'] as $offer) {
+                        if ($offer['price'] <= $item['price']) {
+
+                            curl_setopt($session, CURLOPT_URL, "https://csgo.tm/api/Buy/{$item['classid']}_{$item['instanceid']}/{$offer['price']}/{$data['hash']}/?key=" . $this->config['secret_key']);
+                            $answerAfterBuy = serialize(json_decode(curl_exec($session), true)) . "\n";
+                            echo serialize($answerAfterBuy);
+                            file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'bot.log', $answerAfterBuy, FILE_APPEND);
+
+                            $logString = "[" . date("Y-m-d H:i:s") . "]" . " just bought {$data['name']} for {$offer['price']}\n";
+                            echo $logString;
+                            file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'bot.log', $logString, FILE_APPEND);
+                        }
+                    }
+                }
+            }
+        }
+        curl_close($session);
+    }
+}
+
+if (!is_writable(__DIR__)) {
+    echo "Permission denied\n";
+    die;
+};
+
+if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'bot.log')) {
+    touch(__DIR__ . DIRECTORY_SEPARATOR . 'bot.log');
+}
+
+if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'config.yml')) {
+    echo "config.yml doesn't exist\n";
+    die;
+}
+
+
+
+$config = Spyc::YAMLLoad('config.yml');
+$items = Spyc::YAMLLoad('items.yml');
+
+
+
+unset($items['items']);
+$items = array_chunk($items, ceil(count($items) / 5), false);
+
+
+$pingThread = new PingThread($config);
+$pingThread->start();
+
+$i = 0;
+$threads = [];
+
+foreach ($items as $item) {
+    $threads[$i] = new WorkerThread($item, $config);
+    $threads[$i]->start();
+    $i++;
+}
+
+foreach ($threads as $thread) {
+    $thread->join();
+}
---
+++ readme.txt
@@ -0,0 +1,14 @@
+1. Download this PHP build for Windows - http://windows.php.net/downloads/releases/php-5.6.9-Win32-VC11-x86.zip
+2. Extract archive into c:\php
+3. Download this library http://windows.php.net/downloads/pecl/releases/pthreads/2.0.10/php_pthreads-2.0.10-5.6-ts-vc11-x86.zip
+4. Add pthreadVC2.dll (from php_pthreads-2.0.10-5.6-ts-vc11-x86.zip) to the same directory as php.exe
+5. Add pthreadVC2.dll to c:\windows\system32 directory
+6. Add php_pthreads.dll (from php_pthreads-2.0.10-5.6-ts-vc11-x86.zip) to PHP extension folder eg. c:\php\ext
+7. Find in c:\php file "php.ini-development", and rename it to php.ini
+8. At the end of php.ini, add following line:
+     extension_dir = "ext"
+     extension=php_pthreads.dll
+9. Copy all bot files into c:\php
+10. Open command line in windows and perform commands:
+    cd c:\php
+    php bot.php
