3f4840683a0be6d753aa376507e2a53c7780b0ea
dmitriy <dmitriy.krista@gmail.com>
2015-06-12 08:55:39 +0000

    fixed bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once "Spyc.php";
 
 class PingThread extends Thread {
@@ -39,6 +38,7 @@ class WorkerThread extends Thread {
 
     public function run()
     {
+        date_default_timezone_set('Asia/Dhaka');
         $session = curl_init();
         curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($session, CURLOPT_TIMEOUT, 10);
@@ -53,14 +53,15 @@ class WorkerThread extends Thread {
                 if (json_last_error() != 0) {
                     echo "json_last_error - " . json_last_error() . "\n";
                 } else {
+                    if ($data['min_price'] == -1) {
+                        break;
+                    }
                     foreach($data['offers'] as $offer) {
                         if ($offer['price'] <= $item['price']) {
-
                             curl_setopt($session, CURLOPT_URL, "https://csgo.tm/api/Buy/{$item['classid']}_{$item['instanceid']}/{$offer['price']}/{$data['hash']}/?key=" . $this->config['secret_key']);
                             $answerAfterBuy = serialize(json_decode(curl_exec($session), true)) . "\n";
                             echo serialize($answerAfterBuy);
                             file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'bot.log', $answerAfterBuy, FILE_APPEND);
-
                             $logString = "[" . date("Y-m-d H:i:s") . "]" . " just bought {$data['name']} for {$offer['price']}\n";
                             echo $logString;
                             file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'bot.log', $logString, FILE_APPEND);
@@ -93,9 +94,8 @@ $config = Spyc::YAMLLoad('config.yml');
 $items = Spyc::YAMLLoad('items.yml');
 
 
-
 unset($items['items']);
-$items = array_chunk($items, ceil(count($items) / 5), false);
+$items = array_chunk($items, ceil(count($items) / 4), false);
 
 
 $pingThread = new PingThread($config);
